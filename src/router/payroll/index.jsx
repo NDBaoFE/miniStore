87460983657cb96ba19ebf6b1d759323983b3d8f
MinhKhoi <mkhoi1776@gmail.com>
2023-07-27 17:28:45 +0700

    fix payslip + paid payslip

--- src/router/payroll/index.jsx
+++ src/router/payroll/index.jsx
@@ -8,32 +8,38 @@ import {
   WrapperSum,
 } from "./payrollStyled";
 import { useEffect, useState } from "react";
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 import payrollApi from "../../utils/api/payrollApi";
+import orderManagementApi from "../../utils/api/orderManagementApi";
+import payslip from "../../utils/api/payslipApi";
+import { toastError, toastSuccess } from "../../components/Toast";
 
 const Payroll = () => {
+  const token = localStorage.getItem("Authorization");
+  const [reload, SetReload] = useState(false);
+  const [userPayroll, setUserPayroll] = useState([]);
 
-  const token = localStorage.getItem('Authorization')
 
-  const [userPayroll, setUserPayroll] = useState([])
 
-  useEffect(()=>{
-    async function fetchData(){
-      try{
-        const response = await payrollApi.getPayrollAll(token)
-        console.log(response.data.data)
-        setUserPayroll(response.data.data)
-      
-      }catch(error){
-      console.error(error);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await payrollApi.getPayrollAll(token);
+        console.log(response.data.data);
+        setUserPayroll(response.data.data);
+      } catch (error) {
+        console.error(error);
       }
     }
-    fetchData()
+    fetchData();
+  }, [token, reload, token]);
 
-
-  }, [token])
-
-
+  const handlePayslipPaid = () => {
+    // Refresh the vouchers by triggering a re-render of the VoucherList component
+    // This can be done by incrementing the current page number or any other way to indicate a change
+    SetReload(!reload);
+  };
 
 
   const columns = [
@@ -71,26 +77,45 @@ const Payroll = () => {
     },
   ];
 
+  const confirm = async () => {
+    Modal.confirm({
+      maskClosable: true,
+      title: "Bạn có muốn trả số lương này không?",
+      content: "Khi bạn nhấn đồng ý, lương sẽ được trả cho các nhân viên",
+      okText: "Xác nhận",
+      cancelText: "Huỷ",
+      onOk: async () => {
+        const token = localStorage.getItem("Authorization");
+        const res = await payslip.getPay(token);
+        if (res.status === 200) {
+          toastSuccess("Paid Succesfully");
+        } else {
+          toastError("Paid Failed");
+        }
 
-  const { id } = useParams();
-  // const totalSalaryThisMonth = userPayroll.reduce(
-  //   (total, user) => total + user.salary,
-  //   0
-  // );
-  return (
-    <WrapperPayroll>
-      <WrapperSum>
-        <Title>Total Salary This Month: </Title>
-        <ButtonStyled>Paid</ButtonStyled>
-      </WrapperSum>
+        handlePayslipPaid()
+      },
+      
+    });
+  };
 
-      <PayrollList
-        setUserPayroll={setUserPayroll}
-        userPayroll={userPayroll}
-        columns={columns}
-      ></PayrollList>
-    </WrapperPayroll>
-  );
+ 
+  return  (
+      <WrapperPayroll>
+        <WrapperSum>
+          <Title>Total Salary This Month:  </Title>
+          <ButtonStyled onClick={() => confirm()}>Paid</ButtonStyled>
+        </WrapperSum>
+
+        <PayrollList
+          setUserPayroll={setUserPayroll}
+          userPayroll={userPayroll}
+          reload={reload}
+          columns={columns}
+        ></PayrollList>
+      </WrapperPayroll>
+    )
+  
 };
 
 export default Payroll;
